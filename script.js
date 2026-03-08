a9a0beae734a0f39856c2fe8066bafbbae15641f
CannSahin <cansahin019@gmail.com>
2026-03-08 22:37:51 +0300

    setup state and edited logic for seesaw plank

--- script.js
+++ script.js
@@ -6,28 +6,32 @@ const CONFIG = {
 
 const plank = document.getElementById("seesaw-plank");
 
-plank.addEventListener("click", function(event){
-    const center=300;
-    const distance=event.offsetX-center;
-    const newWeight={
-        weight:5,
-        position:distance
+let state = {
+    placedObjects: []
+};
+
+plank.addEventListener("click", function (event) {
+    const center = 300;
+    const distance = event.offsetX - center;
+    const newWeight = {
+        weight: 5,
+        position: distance
     };
     state.placedObjects.push(newWeight);
     drawBox(newWeight);
     calculateBalance();
 })
 
-function drawBox(obj){
-    const div=document.createElement("div");
-    div.innerText=obj.weight + 'kg';
-    div.className='weight-item';
-    const leftPos=obj.position+300;
-    div.style.left=leftPos+'px';
+function drawBox(obj) {
+    const div = document.createElement("div");
+    div.innerText = obj.weight + 'kg';
+    div.className = 'weight-item';
+    const leftPos = obj.position + 300;
+    div.style.left = leftPos + 'px';
     plank.appendChild(div);
 }
 
-function calculateBalance(){
+function calculateBalance() {
     let leftTorque = 0;
     let rightTorque = 0;
     state.placedObjects.forEach(obj => {
@@ -37,7 +41,7 @@ function calculateBalance(){
             rightTorque += obj.weight * obj.position;
         }
     });
-    let angle =(rightTorque-leftTorque)/100
+    let angle = (rightTorque - leftTorque) / 100
     angle = Math.max(-30, Math.min(30, angle));
     plank.style.transform = `rotate(${angle}deg)`;
 }
@@ -53,3 +57,5 @@ function loadData() {
         calculateBalance();
     }
 }
+
+loadData();
